1f7b3cd214546cd41819507d25fca0f4269c3b5c
huuson-theinfitech <son_tran@theinfitech.com>
2025-01-03 18:39:22 +0700

    Fix: fix port

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true
+  },
   server: {
-    port: 3000 // change here
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:3000'
   }
 })
